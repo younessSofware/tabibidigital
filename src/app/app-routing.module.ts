d271e15dec01c168700d0fb16d7a002c9dad25df
younessSofware <youness3.toyouness18@gmail.com>
2021-06-06 17:54:55 +0100

    this is the important push

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,16 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { HomeComponent } from './modules/home/home.component';
 
 const routes: Routes = [
-  {
-    path: '',
-    component: HomeComponent
-  },
-  {
-    path: 'home',
-    component: HomeComponent
-  }
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,22 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HomeModule } from './modules/home/home.module';
 import { PipesModule } from './pipes/pipes.module';
-import { SingingComponent } from './modules/singing/singing.component';
 import { TestPFEComponent } from './test-pfe/test-pfe.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    SingingComponent,
     TestPFEComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     PipesModule,
-    HomeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
